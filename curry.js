d4bd5cc5c5a1b3adfccaff606f2824d5acbbc570
Prashant Choudhary <prashant.srs@gmail.com>
2020-07-13 21:33:05 +0530

    FIxed bug

--- curry.js
+++ curry.js
@@ -1,4 +1,3 @@
-
 multiply(a, b) {
 return a*b;
 }
@@ -7,10 +6,12 @@ multiply2(3);
 
 const multiply () {
 const args = [...arguments];
-  return multFn () {
+  function multFn () {
     multiply(...args, ...arguments)
   }
   let result = args.reduce((total, item) => total*item)
   multFn.toString = result;
   return multFn;
 }
+
+//https://medium.com/@ishwar.rimal/trickiest-javascript-interview-question-with-solution-73958f99a376
