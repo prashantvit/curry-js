38b41e23c8d89d597ae7ba50070ef48d38618284
Prashant Choudhary <prashant.srs@gmail.com>
2020-07-14 12:02:53 +0530

    fixed

--- curry.js
+++ curry.js
@@ -15,3 +15,19 @@ const args = [...arguments];
 }
 
 //https://medium.com/@ishwar.rimal/trickiest-javascript-interview-question-with-solution-73958f99a376
+
+
+
+function curry(func) {
+
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+
+}
